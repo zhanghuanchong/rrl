ac4c0a727ea3d230f0e79d3b1f1198c65e043cdf
Hans Zhang <devwingcom@gmail.com>
2025-08-31 22:25:20 +0800

    test

--- rrl/test.py
+++ rrl/test.py
@@ -1,4 +1,6 @@
 import torch
 
-print(torch.backends.mps.is_available())
-print(torch.backends.mps.is_built())
+print('mps available: ' + str(torch.backends.mps.is_available()))
+print('mps builtin: ' + str(torch.backends.mps.is_built()))
+
+print('cuda builtin: ' + str(torch.backends.cuda.is_built()))
